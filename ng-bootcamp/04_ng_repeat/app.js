32db3d4a219d1d996c0f794dcd31e2ba950a2d1e
joelhooks <joelhooks@gmail.com>
2015-12-01 10:53:32 -0600

    add a method to add items to a repeated array

--- ng-bootcamp/04_ng_repeat/app.js
+++ ng-bootcamp/04_ng_repeat/app.js
@@ -2,6 +2,11 @@ angular.module('app', [])
   .controller('AppCtrl', function() {
     var app = this;
 
+    app.addFruitToList = function(newFruit) {
+      app.fruits.push(newFruit);
+      app.newFruit = '';
+    };
+
     app.fruits = [
       'Apples',
       'Oranges',
