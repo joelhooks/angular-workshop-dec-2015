edb7f1a8952fa5214df4146ac799aac38890da60
joelhooks <joelhooks@gmail.com>
2015-12-01 10:26:48 -0600

    use ng-repeat to repeat an array of items

---
+++ ng-bootcamp/04_ng_repeat/app.js
@@ -0,0 +1,14 @@
+angular.module('app', [])
+  .controller('AppCtrl', function() {
+    var app = this;
+
+    app.fruits = [
+      'Apples',
+      'Oranges',
+      'Bananas',
+      'Pears',
+      'Pineapples',
+      'Skittles'
+    ]
+  })
+;
